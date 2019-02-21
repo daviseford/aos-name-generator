348e0bb1786ca2629d6b6c783731149c76098d11
Davis Ford <davis.e.ford@gmail.com>
2019-02-21 18:47:20 -0500

    finished MVP!

--- handler.js
+++ handler.js
@@ -4,10 +4,7 @@ const lib_index = require('./lib/index')
 const root = async (event, context) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+    body: 'Hello! Visit /seraphon if you care!',
   };
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
